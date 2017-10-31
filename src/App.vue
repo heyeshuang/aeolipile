2fb3253524b9613a624797b42194931e226293ab
He Yeshuang <yeshuanghe@gmail.com>
2017-10-31 13:53:15 +0800

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,52 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <el-button @click="startHacking">Start</el-button>
-    </div>
+    <el-row type="flex" justify="center">
+      <el-col class="center">
+    <el-card>
+    <el-container>
+      <el-header><h2>Some Pandas: 另一只熊猫</h2></el-header>
+      <el-main>
+        <vue-markdown></vue-markdown>
+      </el-main>
+      <el-footer>Bamboo</el-footer>
+    </el-container>
+    </el-card></el-col></el-row>
   </div>
 </template>
 
 <script>
+// import Typography from 'typography'
+// import customTheme from './typography-theme-custom.js'
 export default {
   methods: {
-    startHacking () {
-      this.$notify({
-        title: 'It works!',
-        type: 'success',
-        message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
-        duration: 5000
-      })
-    }
+    // startHacking () {
+    //   this.$notify({
+    //     title: 'It works!',
+    //     type: 'success',
+    //     message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
+    //     duration: 5000
+    //   })
+    // },
+    // introTypo () {
+      // const typography = new Typography(customTheme)
+      // return typography.toString()
+    // }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Helvetica, sans-serif;
-  text-align: center;
+  /* font-family: Helvetica, sans-serif;
+  text-align: center; */
+}
+.center {
+  flex-basis: 960px;
+}
+.el-card {
+  margin: 20px;
+}
+.el-header {
+  text-align: left
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
+import './assets/typebase.css'
+import VueMarkdown from 'vue-markdown'
 
 Vue.use(ElementUI)
+Vue.use(VueMarkdown)
 
 new Vue({
   el: '#app',
